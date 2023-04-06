1894629c08ec834210c58f8eb7abf22cfb2d460a
hzlinyiyu <linyiyu1992@gmail.com>
2023-04-06 14:12:27 +0800

    more

--- src/adapter.rs
+++ src/adapter.rs
@@ -257,7 +257,7 @@ unsafe fn get_adapter(adapter_addresses_ptr: PIP_ADAPTER_ADDRESSES) -> Result<Ad
 }
 
 unsafe fn socket_address_to_ipaddr(socket_address: &SOCKET_ADDRESS) -> IpAddr {
-    let (_, sockaddr) = socket2::SockAddr::init(|storage, length| {
+    let (_, sockaddr) = socket2::SockAddr::try_init(|storage, length| {
         let sockaddr_length = usize::try_from(socket_address.iSockaddrLength).unwrap();
         assert!(sockaddr_length <= std::mem::size_of_val(&*storage));
         let dst: *mut u8 = storage.cast();
