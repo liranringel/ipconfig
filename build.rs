4a4fce7769561226227bfc657c78a1e84b8586eb
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2026-02-21 21:19:23 +0100

    generate bindings at build time
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,18 @@
+#[cfg(windows)]
 fn main() {
     println!("cargo:rustc-link-lib=iphlpapi");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
+
+#[cfg(not(windows))]
+fn main() {}
