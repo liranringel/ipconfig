0f2ee47c700284be122aec46cc38c59696a1ce27
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-03-24 16:47:33 +0100

    Replace winreg with windows-registry

--- src/computer.rs
+++ src/computer.rs
@@ -2,81 +2,42 @@
 
 use std::string::String;
 
-use windows_sys::Win32::System::Registry::HKEY;
-use winreg::enums::HKEY_LOCAL_MACHINE;
-use winreg::enums::KEY_READ;
-use winreg::types::FromRegValue;
-use winreg::RegKey;
+use windows_registry::LOCAL_MACHINE;
+use windows_result::HRESULT;
 
 use crate::error::*;
 
-/// Returns a value from the registry, and returns a default if it doesn't exist.
-fn get_value<T: FromRegValue>(
-    predef: HKEY,
-    subkey: &str,
-    value_name: &str,
-    default: T,
-) -> ::std::io::Result<T> {
-    let key = RegKey::predef(predef);
-
-    let value: T = match key.open_subkey_with_flags(subkey, KEY_READ) {
-        Ok(key) => match key.get_value(value_name) {
-            Ok(value) => value,
-            Err(err) => match err.kind() {
-                ::std::io::ErrorKind::NotFound => default,
-                _ => return Err(err),
-            },
-        },
-        Err(err) => match err.kind() {
-            ::std::io::ErrorKind::NotFound => default,
-            _ => return Err(err),
-        },
-    };
-
-    Ok(value)
-}
-
-const TCPIP_PARAMETERS_KEY_PATH: &str = "SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters";
-
 /// Returns the DNS suffix search list for the network connection used by the computer.
 pub fn get_search_list() -> Result<Vec<String>> {
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let params_key = hklm.open_subkey_with_flags(TCPIP_PARAMETERS_KEY_PATH, KEY_READ)?;
-    let search_list: ::std::io::Result<String> = params_key.get_value("SearchList");
-    if let Ok(search_list) = search_list {
-        let search_list: Vec<String> = search_list
-            .split(',')
-            .map(std::string::ToString::to_string)
-            .collect();
-        Ok(search_list)
-    } else {
-        Ok(vec![])
+    match LOCAL_MACHINE
+        .get_string("SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters\\SearchList")
+    {
+        Ok(search_list) => Ok(search_list.split(',').map(|s| s.to_owned()).collect()),
+        Err(err) if err.code() == E_FILE_NOT_FOUND => Ok(vec![]),
+        Err(err) => Err(err.into()),
     }
 }
 
 /// Returns the computer domain name (if any).
 /// Returns `None` if the computer does not belong to a domain.
 pub fn get_domain() -> Result<Option<String>> {
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let params_key = hklm.open_subkey_with_flags(TCPIP_PARAMETERS_KEY_PATH, KEY_READ)?;
-    let domain: String = params_key.get_value("Domain")?;
-
-    let domain = if domain.is_empty() {
-        None
-    } else {
-        Some(domain)
-    };
-    Ok(domain)
+    match LOCAL_MACHINE.get_string("SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters\\Domain")
+    {
+        Ok(domain) => Ok((!domain.is_empty()).then_some(domain)),
+        Err(err) if err.code() == E_FILE_NOT_FOUND => Ok(None),
+        Err(err) => Err(err.into()),
+    }
 }
 
 /// Returns `true` if the computer is configured to use the round robin strategy.
 /// Otherwise, returns `false`.
 pub fn is_round_robin_enabled() -> Result<bool> {
-    let rotate: u32 = get_value(
-        HKEY_LOCAL_MACHINE,
-        "SYSTEM\\CurrentControlSet\\Services\\DNS\\Parameters",
-        "RoundRobin",
-        1, // The default is 1 according to msdn
-    )?;
-    Ok(rotate != 0)
+    match LOCAL_MACHINE.get_u32("SYSTEM\\CurrentControlSet\\Services\\DNS\\Parameters\\RoundRobin")
+    {
+        Ok(value) => Ok(value != 0),
+        Err(err) if err.code() == E_FILE_NOT_FOUND => Ok(true), // The default is 1 according to msdn
+        Err(err) => Err(err.into()),
+    }
 }
+
+const E_FILE_NOT_FOUND: HRESULT = HRESULT(0x80070002_u32 as i32);
